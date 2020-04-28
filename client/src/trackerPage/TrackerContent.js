5960d4c45537d94db35faad3eaf401aea8f914cc
Takuji Okubo <takuji.okubo@gmail.com>
2020-04-28 13:56:11 +1000

    Show content if logged in

--- client/src/trackerPage/TrackerContent.js
+++ client/src/trackerPage/TrackerContent.js
@@ -2,19 +2,24 @@ import React, {useContext} from 'react';
 import Topbar from '../common/Topbar';
 import { LoginContext } from '../contexts/LoginContext';
 
-
 function TrackerContent() {
-  let login = useContext(LoginContext);
-  console.log("****loggedIn**** : ", login)
-  return (
+  let loginCtx = useContext(LoginContext);
+  let { loggedIn, username } = loginCtx;
+  //console.log("****logIn**** loggedIn, username : ", loggedIn, username)
+  return ( 
     <div id="content-wrapper" class="d-flex flex-column">
       <div id="content">
           <Topbar />
-          <div>
-            <h1>This page is private. </h1>
-            <h3>Login Status: {login.loggedIn.toString()} Username: {login.username}</h3> 
-            Tracker content will be inserted here.<br/>
-          </div>
+          {loggedIn ? (
+            <div>
+              <h1>You are logged in as {username}. </h1>
+              Tracker content will be inserted here.<br/>
+            </div>
+          ) : (
+            <div>
+              <h1>You are not authorized to view this page.</h1>
+            </div>
+          )}
       </div>
     </div>
   );
--- client/src/trackerPage/index.js
+++ client/src/trackerPage/index.js
@@ -1,13 +1,8 @@
 import React, {useContext} from 'react';
 import Sidebar from '../common/Sidebar';
 import TrackerContent from './TrackerContent';
-import { LoginContext } from '../contexts/LoginContext';
 
 function TrackerPage() {
-  let login = useContext(LoginContext);
-
-  console.log("****loggedIn**** : ", login)
-
   return (
     <div id="wrapper">
         <Sidebar />
