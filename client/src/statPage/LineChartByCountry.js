821822469202b3a39b2a10f8ddaf9f1e98a62bbf
Takuji Okubo <takuji.okubo@gmail.com>
2020-05-01 16:24:45 +1000

    Added area chart

--- client/src/statPage/LineChartByCountry.js
+++ client/src/statPage/LineChartByCountry.js
@@ -1,25 +1,147 @@
-import React from 'react';
-import useScript from '../customhook/useScript';
+import React, {Component} from 'react';
+import Chart from 'chart.js';
+import axios from 'axios';
 
-function LineChartByCountry() {
+class LineChartByCountry extends Component {
+    chartRef = React.createRef();
 
-    useScript("js/demo/chart-area-demo.js");
+    componentDidMount(){
+        this.getTimelineDataAus() 
+        .then(result => this.createChart(result));
+    }
 
-    return (
-        <div className="card shadow mb-4 h-100">
-            {/* Card Header - Dropdown */}
-            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 className="m-0 font-weight-bold text-primary">Number of Cases</h6>
-                {/*-- Dropdown menu has been removed from here -- */}
-            </div>
-            {/* Card Body */}
-            <div className="card-body">
-                <div className="chart-area">
-                <canvas id="myAreaChart" />
+    getTimelineDataAus() {
+        return new Promise((resolve, reject) => { 
+            const coronaAustUrl = "https://corona-api.com/countries/au";
+            var timeline;
+            axios.get(coronaAustUrl)
+            .then(res => {
+                // console.log("getTimelineDataAus() res", res.data.data.timeline)
+                resolve(res.data.data.timeline);
+            })
+            .catch(err=>{
+                reject(err);
+            })
+        })
+    }
+
+    createChart(input){
+        console.log("createChart()", input);
+
+        var confirmedNumbers = [];
+        var dates = [];
+        for (let i=0; i<input.length ;i++){
+            confirmedNumbers.unshift(input[i].confirmed);
+            dates.unshift(input[i].date);
+        }
+        console.log(dates);
+        const myChartRef = this.chartRef.current.getContext("2d");  
+        var myLineChart = new Chart(myChartRef, {
+            type: 'line',
+            data: {
+              labels: dates,
+              datasets: [{
+                label: "Label",
+                lineTension: 0.1,
+                backgroundColor: "rgba(78, 115, 223, 0.05)",
+                borderColor: "rgba(78, 115, 223, 1)",
+                pointRadius: 2,
+                pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointBorderColor: "rgba(78, 115, 223, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: confirmedNumbers,
+              }],
+            },
+            options: {
+              maintainAspectRatio: false,
+              layout: {
+                padding: {
+                  left: 10,
+                  right: 25,
+                  top: 25,
+                  bottom: 0
+                }
+              },
+              scales: {
+                xAxes: [{
+                  time: {
+                    unit: 'date'
+                  },
+                  gridLines: {
+                    display: false,
+                    drawBorder: false
+                  },
+                  ticks: {
+                    maxTicksLimit: 7
+                  }
+                }],
+                yAxes: [{
+                  ticks: {
+                    maxTicksLimit: 5,
+                    padding: 10,
+                    // Include a dollar sign in the ticks
+                    // callback: function(value, index, values) {
+                    //   return '$' + number_format(value);
+                    // }
+                  },
+                  gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                  }
+                }],
+              },
+              legend: {
+                display: false
+              },
+              tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                intersect: false,
+                mode: 'index',
+                caretPadding: 10,
+                // callbacks: {
+                //   label: function(tooltipItem, chart) {
+                //     var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                //     return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+                //   }
+                // }
+              }
+            }
+          });
+    }
+
+    render(){
+        return (
+            <div className="card shadow mb-4">
+                {/* Card Header - Dropdown */}
+                <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 className="m-0 font-weight-bold text-primary">Confired Cases in Australia</h6>
+                    {/*-- Dropdown menu has been removed from here -- */}
                 </div>
-            </div>
-        </div>        
-    )
+                {/* Card Body */}
+                <div className="card-body">
+                    <div className="chart-area">
+                    <canvas id="myAreaChart"  ref={this.chartRef} />
+                    </div>
+                </div>
+            </div>        
+        )
+    }
 }
 
 export default LineChartByCountry;
