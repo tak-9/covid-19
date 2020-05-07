5a8217c9a4705970c40156eab9accef76b4547ee
Takuji Okubo <takuji.okubo@gmail.com>
2020-05-07 17:31:21 +1000

    Removed debug

--- client/src/trackerPage/Feed.js
+++ client/src/trackerPage/Feed.js
@@ -33,7 +33,6 @@ function Feed() {
                 let dayName = new Date(item.day).toLocaleDateString("en-AU", {weekday: 'long'});
                 let monthName = new Date(item.day).toLocaleDateString("en-AU", {month: 'long'});
                 let dateStr = dayName + ", " + new Date(item.day).getDate() + " " + monthName;
-                console.log("----item.username", item);
                 return <Activity key={i} 
                             username={feedData.username}
                             date={dateStr}
