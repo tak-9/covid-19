7bca5705b7258d6211099c71d4556baf672edc15
Takuji Okubo <takuji.okubo@gmail.com>
2020-05-13 16:26:24 +1000

    troubleshooting heroku

--- client/src/allCountriesPage/WorldCard.js
+++ client/src/allCountriesPage/WorldCard.js
@@ -102,6 +102,7 @@ function WorldCard() {
                 <h6 className="m-0 font-weight-bold text-primary">Worldwide Cases</h6>
             </div>
             <div className="card-body">
+                columns
                 {console.log("columns", columns)}
                 {((typeof allCountries!=='undefined') && (typeof columns!=='undefined')) ? 
                     <ReactTable columns={columns} data={allCountries} /> : <span>Loading...</span>}                
