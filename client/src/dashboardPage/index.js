d6fb0adefffedaba8ea4f7695b8544a0f9f61801
Takuji Okubo <takuji.okubo@gmail.com>
2020-05-17 17:12:26 +1000

    For netlify

--- client/src/dashboardPage/index.js
+++ client/src/dashboardPage/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../common/Sidebar';
 import Content from './Content';
 import useScript from '../customhook/useScript';
 import ContentWrapper from '../common/ContentWrapper';
 import Topbar from '../common/Topbar';
+import axios from 'axios';
+
 
 function DashboardPage() {
   useScript("js/sb-admin-2.js");
+
+  useEffect(()=>{
+        // Wakeup Heroku when accessed from Netlify
+        if (process.env.NODE_ENV === "netlify") {
+            axios.get("https://covid19-au.herokuapp.com/img/login.jpg");
+        }
+    }
+  );
+    
   return (
     <div id="wrapper">
         <Sidebar currentPage="dashboard"/>
